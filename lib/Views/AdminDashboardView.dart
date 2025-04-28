3a5be9e51df91711c7e9b626b9ca02e762509a89
Kasım Enes Karamürsel <eneskaramursel@gmail.com>
2025-04-28 18:58:56 +0300

    admin dashboard account page improved

--- lib/Views/AdminDashboardView.dart
+++ lib/Views/AdminDashboardView.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e44d3e97ab2082a28a98a817b1f898f92f2f18beb81d6ef8fd2de1f5dfb84c5
-size 22776
+oid sha256:0913b8c17818d2ce32a476d654f9c05871b621b717cf2a656578a22c2e01a3d3
+size 31492

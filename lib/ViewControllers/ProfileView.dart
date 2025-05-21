8155e8c72a71b6a9648fd38b5e8572826ccf90ff
Kasım Enes Karamürsel <eneskaramursel@gmail.com>
2025-05-21 15:23:01 +0300

    signout cancel button bug fix

--- lib/ViewControllers/ProfileView.dart
+++ lib/ViewControllers/ProfileView.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:86080961c52e51d9c8874b4ea7e10c31a660bd6a3d75904df85ee28f75e44a3d
-size 9279
+oid sha256:fdffda27c32e71b0b25c2f62c8a2a60a70de98e3314d6df54fab83ce499c5b54
+size 9719

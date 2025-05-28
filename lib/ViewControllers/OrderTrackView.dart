698a51622bfa313a5fd9cdb67022fd38b46ae6a7
Kayraakbas <kayraakbas7gmail.com>
2025-05-28 19:36:42 +0300

    order status update fix

--- lib/ViewControllers/OrderTrackView.dart
+++ lib/ViewControllers/OrderTrackView.dart
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c433d12a204884bd8e673ac889650edb1ca023fc89415400d13a2e73fd6178ea
-size 14375
+oid sha256:52beb84a296d21fe5c16a8d43fbaf065f5cc33dd7c24d685374cb7617b7c8f14
+size 15283
